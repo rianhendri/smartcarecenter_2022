f107129e0c05232a21d6ed2bc5686d0394876f54
delapansoftproject <rian@delapansoft.com>
2020-08-26 15:00:43 +0700

    Tambah activity_add__po__item__list.xml, activity_add_details_po.xml, activity_add_details_po_view.xml, activity_chargeable.xml

---
+++ app/src/main/java/com/smartcarecenter/AddDetailsPo.java
@@ -0,0 +1,14 @@
+package com.smartcarecenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddDetailsPo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_details_po);
+    }
+}
---
+++ app/src/main/java/com/smartcarecenter/Add_Po_Item_List.java
@@ -0,0 +1,14 @@
+package com.smartcarecenter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Add_Po_Item_List extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add__po__item__list);
+    }
+}
